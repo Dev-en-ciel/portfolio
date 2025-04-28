ffcb4b7fdea807df19ad9dad104fa75dfe3cd227
Devenciel <89196322+Dev-en-ciel@users.noreply.github.com>
2025-04-28 21:34:25 +0200

    up

--- js/index.js
+++ js/index.js
@@ -2,6 +2,26 @@
 document.querySelectorAll('.name li').forEach((letter, index) => {
     letter.style.animationDelay = `${index * 0.1}s`;
 });
+// menu burger
+document.addEventListener('DOMContentLoaded', function() {
+    const menuBtn = document.querySelector('.mobile-menu-btn');
+    const menu = document.querySelector('.desktop-menu');
+    
+    menuBtn.addEventListener('click', function() {
+        menu.classList.toggle('active');
+        this.innerHTML = menu.classList.contains('active') ? '&times;' : '&#9776;';
+        document.body.style.overflow = menu.classList.contains('active') ? 'hidden' : '';
+    });
+    
+    // Fermer le menu en cliquant sur un lien
+    document.querySelectorAll('.desktop-menu a').forEach(link => {
+        link.addEventListener('click', () => {
+            menu.classList.remove('active');
+            menuBtn.innerHTML = '&#9776;';
+            document.body.style.overflow = '';
+        });
+    });
+});
 
 // Gestion du bouton de retour en haut de la page
 const returnButton = document.querySelector('.return-to-top');
